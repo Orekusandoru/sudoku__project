d8f087c414047a985dd7eb3d189f824204dbfb13
DESKTOP-FK2D82I\SaKe <sasha.g360@gmail.com>
2023-05-02 02:38:04 +0300

    create app

---
+++ sudoku-site/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+    <h1>s2s22d2s22</h1>
+    <Button variant = "primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
