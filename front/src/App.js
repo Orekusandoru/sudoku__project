f4f43cde447332d09c1e7ca2113bbc768e6623b4
DESKTOP-FK2D82I\SaKe <sasha.g360@gmail.com>
2023-05-08 19:46:42 +0300

    create first page design

---
+++ front/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+       
+        <a href="" className="logo" >
+          <img  src="../logo192.png" width="30" height="30" />   
+          Sudoku
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+
+        </nav>
+      </header>
+      <div className="post">
+
+        
+
+        <h1></h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
