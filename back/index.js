7aea0eb2f3fbdebcbb368e517f8201bbb3b4fae8
DESKTOP-FK2D82I\SaKe <sasha.g360@gmail.com>
2023-05-15 15:59:43 +0300

    Added test input login and password

--- back/index.js
+++ back/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const cors =require('cors');
+
+
 const app = express();
 
+app.use(cors())
+app.use(express.json())
+
 app.get('/test',(req,res)=>{
-    res.json('test ok');
+   
+    res.json("ok");
+
+   
 });
+app.post('/register',(req,res)=>{
+    const {username,password} =req.body;
+    res.json({requestData:{username,password}});
+
+   
+});
+
+
+
 
 app.listen(4000);
